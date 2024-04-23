be709f5d6c875c068fa46faf463978ef9a650812
Opeyemi0001 <opeyemionanuga40@gmail.com>
2024-04-23 16:23:16 +0100

    new changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter } from 'react-router-dom'
+import StoreContextProvider from './Context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+
+  </HashRouter>
 )
